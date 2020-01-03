6d0fdc605fbff06574a3c3e04d96e67854bc3997
Iury Adones <iuryadones@gmail.com>
2020-01-03 17:05:04 -0300

    Updates

---
+++ precache-manifest.2e5d1127e7ded4df2cf43131f26594c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04988240a83581bfbd1b63cfaf156b6",
+    "url": "/react-getting-started/index.html"
+  },
+  {
+    "revision": "a9c8e6c1a775e8775ed0",
+    "url": "/react-getting-started/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ecd3005e562dcae45e12",
+    "url": "/react-getting-started/static/js/2.d23f6904.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-getting-started/static/js/2.d23f6904.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9c8e6c1a775e8775ed0",
+    "url": "/react-getting-started/static/js/main.b11ed922.chunk.js"
+  },
+  {
+    "revision": "d86730c0ad15fa053117",
+    "url": "/react-getting-started/static/js/runtime-main.d351a06a.js"
+  }
+]);
