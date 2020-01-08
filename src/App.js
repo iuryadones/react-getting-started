3bdd9e60b49cb785c593ab1c4d046b5823d17649
Iury Adones <iuryadones@gmail.com>
2020-01-08 16:55:10 -0300

    [master] Componentes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import logo from './logo.svg'
+import github from './github.svg'
 import Card from './components/Card'
 import Name from './apps/App1'
 import NameDict from './apps/App2'
@@ -11,9 +13,24 @@ function App() {
     {name: 'App 2 - NameDict', cmp: <NameDict />},
     {name: 'App 3 - NameConditional', cmp: <NameConditional />},
   ]
+
   return (
-    <Card apps={apps}/>
+    <div>
+      <div
+        style={{'width': "10%",
+                'float': 'left',
+                'text-align': 'center' }}>
+        <a href='https://github.com/iuryadones/react-getting-started/'>
+          <img src={github} alt="GitHub" width='80%' />
+        </a>
+      </div>
+      <div style={{'width': '90%',
+                   'float': 'right'}}>
+        <Card apps={apps}/>
+      </div>
+    </div>
   )
 }
+
 
 export default App
